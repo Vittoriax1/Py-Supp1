d5a475309bc90d5abbd7805f063b1adbc99e8a55
Vittoriax1 <99565350+Vittoriax1@users.noreply.github.com>
2022-07-01 16:06:44 -0400

    Add files via upload

---
+++ Supp1.py
@@ -0,0 +1,32 @@
+# Supplemental Exercises #1, created by Jennifer Souvannasing
+
+print(' ')
+# Concatenation Strings Exercise
+print('Here is the concantenating strings exercise.')
+animal = "Llama"
+vegetable = "Broccoli"
+mineral = "Platinum"
+print('Here is an animal, a vegetable and a mineral: ')
+print(animal + " " + vegetable + " " + mineral)
+print(' ')
+print('-' * 50)
+
+# Simple printing
+print(' ')
+print('Here is the first simple printing exercise.')
+print(' ')
+print('Here is a different animal, vegetable and mineral:')
+animal2 = "Emu"
+vegetable2 = "Parsnips"
+mineral2 = "Beryl"
+print(animal2, vegetable2, mineral2)
+print(' ')
+print('-' * 50)
+
+# Some more simple printing
+print(' ')
+print('Here is more simple printing.')
+team = "Mighty Ducks"
+status = "rule"
+punctuation = "!"
+print(team, status, punctuation)
